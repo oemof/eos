069a479d8c5a5411ceb5a41f118bb243d5487a50
Caroline Moeller <caroline.moeller@rl-institut.de>
2016-10-10 15:22:56 +0200

    Add options to plot

--- plots/quartier_plots.py
+++ plots/quartier_plots.py
@@ -7,8 +7,12 @@ Usage: quartier_plots.py [options]
 Options:
 
   -c, --cost=COST          The cost scenario. [default: 1]
-      --num-regions=NUM    Number of regions. [default: 84]
-      --profile=PROFILE    Choose between summer, winter, day and night.
+      --num-hh=NUM    Number of households. [default: 84]
+      --year=YEAR          Weather year. [default: 2010]
+      --ssr=SSR            Self-sufficiency degree. [default: None]
+      --profile=PROFILE    Choose between random, summer, winter, day and night.
+                           [default: random]
+  -h, --help               Display this help.
 
 '''
 
@@ -17,12 +21,15 @@ Options:
 ###############################################################################
 import pickle
 from docopt import docopt
+import numpy as np
 
 
 def get_results(arguments):
     results = pickle.load(open('../results/quartier_results_' +
-                               str(arguments['--num-regions']) + '_' +
+                               str(arguments['--num-hh']) + '_' +
                                str(arguments['--cost']) + '_' +
+                               str(arguments['--year']) + '_' +
+                               str(arguments['--ssr']) + '_' +
                                str(arguments['--profile']) + '.p', 'rb'))
 
     return results
@@ -35,3 +42,10 @@ if __name__ == '__main__':
     print('check_ssr: ', results['check_ssr'])
     print('storage_cap: ', results['storage_cap'])
     print('objective: ', results['objective'])
+    print(np.arange(1, 85))
+   #  for house in np.arange(1, 85):
+   #      print(house)
+   #      print('pv_max_' + str(house) + ':',
+   #              results['pv_max_house_' + str(house)])
+    # print('hh: ', results['hh'])
+    # print('pv_max: ', results['pv_max_house_1' ])
