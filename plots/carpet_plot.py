4d6eba2bc0ee3aa1fabd0eb2bdce56edcf6c8851
Caroline Moeller <caroline.moeller@rl-institut.de>
2016-11-09 17:15:18 +0100

    Improve plot

--- plots/carpet_plot.py
+++ plots/carpet_plot.py
@@ -22,6 +22,7 @@ Options:
 ###############################################################################
 from docopt import docopt
 import numpy as np
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import quartier_plots
 
@@ -52,6 +53,7 @@ class Carpet:
                    aspect='auto')
         plt.xlabel('Days of year')
         plt.ylabel('Hours of day')
+        # plt.clim(-1800, 300)
         clb = plt.colorbar()
         clb.set_label(res_name)
 
@@ -69,14 +71,47 @@ class Line:
     def __init__(self, name=None):
         self.name = name
 
-    def line_plot(res, res_name, show=True):
+    def line_plot(res_name, show=True, **kwargs):
         '''
         Empty docstring.
         '''
         fig = plt.figure()
-        plt.plot(res)
+
+        lw = 2
+
+        if kwargs.get('wind') is not None:
+            plt.plot(np.arange(0, 8760), kwargs.get('wind'), linewidth=lw)
+        if kwargs.get('pv') is not None:
+            plt.plot(np.arange(0, 8760), kwargs.get('pv'))
+        if kwargs.get('demand') is not None:
+            plt.plot(np.arange(0, 8760), kwargs.get('demand'))
+        if kwargs.get('residual_1') is not None:
+            plt.plot(np.arange(0, 8760), kwargs.get('residual_1'),
+                    linewidth=lw,
+                    label=kwargs.get('label_res_1'))
+        if kwargs.get('residual_2') is not None:
+            plt.plot(np.arange(0, 8760), kwargs.get('residual_2'),
+                    linewidth=lw,
+                    label=kwargs.get('label_res_2'))
+        if kwargs.get('residual_3') is not None:
+            plt.plot(np.arange(0, 8760), kwargs.get('residual_3'),
+                    linewidth=lw,
+                    label=kwargs.get('label_res_3'))
+        if kwargs.get('residual_4') is not None:
+            plt.plot(np.arange(0, 8760), kwargs.get('residual_4'), linewidth=lw)
+
+        plt.axhline(0, color='black')
+
         plt.xlabel('Hours of the year')
         plt.ylabel(res_name)
+        # plt.legend(kwargs.keys())
+
+        # res_1_patch = mpatches.Patch(color='red', label='OS')
+        # res_2_patch = mpatches.Patch(color='blue', label='LKOS')
+        # res_3_patch = mpatches.Patch(color='green', label='OS + LKOS')
+
+        # plt.legend(handles=[res_1_patch, res_2_patch, res_3_patch])
+        plt.legend()
 
         if show:
             plt.show()
